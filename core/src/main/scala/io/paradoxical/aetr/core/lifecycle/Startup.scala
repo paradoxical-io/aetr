5e658b1afbf033b6327ad109bf3e99b53842fee7
anton <akropp@gmail.com>
2018-03-29 11:31:16 -0700

    Comments

--- core/src/main/scala/io/paradoxical/aetr/core/lifecycle/Startup.scala
+++ core/src/main/scala/io/paradoxical/aetr/core/lifecycle/Startup.scala
@@ -3,6 +3,13 @@ package io.paradoxical.aetr.core.lifecycle
 import io.paradoxical.aetr.core.execution.Advancer
 import javax.inject.Inject
 
+/**
+ * On startup try and advance whatever we can.
+ *
+ * If the service crashes, or has other transient errors
+ * this should catch any orphaned failures
+ * @param advancer
+ */
 class Startup @Inject()(advancer: Advancer) {
   def start(): Unit = {
     advancer.advanceAll()
