35b64a252096bf3b0fda39c95f97511aa18185fc
anton <akropp@gmail.com>
2018-03-30 12:15:28 -0700

    Adding db ordering tests

--- core/src/test/scala/io/paradoxical/aetr/PostgresDbTestBase.scala
+++ core/src/test/scala/io/paradoxical/aetr/PostgresDbTestBase.scala
@@ -18,4 +18,22 @@ class DbTests extends PostgresDbTestBase {
 
     db.getTree(parent.id).waitForResult() shouldEqual parent
   }
+
+  it should "rebuild children on change" in withDb { injector =>
+    val db = injector.instance[StepDb]
+
+    val leaf1 = Action(name = NodeName("leaf1"))
+    val leaf2 = Action(name = NodeName("leaf2"))
+    val leaf3 = Action(name = NodeName("leaf3"))
+    val parent = SequentialParent(name = NodeName("parent"), children = List(leaf1, leaf2))
+
+    db.upsertStep(parent).waitForResult()
+
+    // change the order and add a new one
+    val parentWithoutChild1 = parent.copy(children = List(leaf2, leaf1, leaf3))
+
+    db.upsertStep(parentWithoutChild1).waitForResult()
+
+    db.getTree(parent.id).waitForResult() shouldEqual parentWithoutChild1
+  }
 }
