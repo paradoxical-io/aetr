49c5612647a6a493b1caea716a7abffd040eaed0
anton <akropp@gmail.com>
2018-04-03 09:46:11 -0700

    Starting async poll thread and fixing dispatch lock per run

--- core/src/main/scala/io/paradoxical/aetr/core/lifecycle/Startup.scala
+++ core/src/main/scala/io/paradoxical/aetr/core/lifecycle/Startup.scala
@@ -1,5 +1,6 @@
 package io.paradoxical.aetr.core.lifecycle
 
+import io.paradoxical.aetr.core.config.ServiceConfig
 import io.paradoxical.aetr.core.db.dao.StepDb
 import io.paradoxical.aetr.core.execution.{AdvanceQueuer, Advancer}
 import io.paradoxical.aetr.core.model.RunState
@@ -16,16 +17,70 @@ import scala.concurrent.ExecutionContext
 class Startup @Inject()(
   stepsDb: StepDb,
   advanceQueuer: AdvanceQueuer,
+  serviceConfig: ServiceConfig,
   advancer: Advancer
 )(implicit executionContext: ExecutionContext) {
+  protected val logger = org.slf4j.LoggerFactory.getLogger(getClass)
+
   def start(): Unit = {
-    stepsDb.findUnlockedRuns(RunState.Pending).map(_.map(advanceQueuer.enqueue)).waitForResult()
+    enqueuePending()
 
+    startDequeueThread()
+
+    startPollThread()
+
+    logger.info("Dequeue and polling threads started")
+  }
+
+  private def enqueuePending(): Unit = {
+    try {
+      val pendingIds = stepsDb.findUnlockedRuns(RunState.Pending).waitForResult()
+
+      if(pendingIds.nonEmpty) {
+        logger.info(s"Processing ${pendingIds.size} pending requests")
+
+        pendingIds.map(advanceQueuer.enqueue)
+      }
+    } catch {
+      case ex: Exception =>
+        logger.warn("Unable to enqueue pending!", ex)
+    }
+  }
+
+  private def startPollThread(): Unit = {
     val dequeueThread = new Thread(() => {
-      while(true) {
-        val dequeue = advanceQueuer.dequeue()
+      while (true) {
+        try {
+          enqueuePending()
 
-        advancer.advance(dequeue)
+          Thread.sleep(serviceConfig.pendingPollTime.toMillis)
+        } catch {
+          case ex: Exception =>
+            logger.error("Unknown error during polling for pending!", ex)
+
+            Thread.sleep(serviceConfig.pendingPollTime.toMillis)
+        }
+      }
+    })
+
+    dequeueThread.setDaemon(true)
+    dequeueThread.setName("Pending-Polling-thread")
+    dequeueThread.start()
+  }
+
+  private def startDequeueThread(): Unit = {
+    val dequeueThread = new Thread(() => {
+      while (true) {
+        try {
+          val dequeue = advanceQueuer.dequeue()
+
+          advancer.advance(dequeue)
+        } catch {
+          case ex: Exception =>
+            logger.error("Unknown error during advancement!", ex)
+
+            Thread.sleep(serviceConfig.threadSleepOnDequeueError.toMillis)
+        }
       }
     })
 
