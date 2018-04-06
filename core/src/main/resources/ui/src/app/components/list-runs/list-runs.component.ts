aa6468a31f41ad3dd3fd909f3b558a5e5fc100c2
anton <akropp@gmail.com>
2018-04-06 15:25:02 -0700

    Initial child mappings, no cycle detection yet

--- core/src/main/resources/ui/src/app/components/list-runs/list-runs.component.ts
+++ core/src/main/resources/ui/src/app/components/list-runs/list-runs.component.ts
@@ -1,71 +1,16 @@
-// import { Component, OnInit } from '@angular/core';
-// import {ApiService} from "../../services/api.service";
-//
-// @Component({
-//   selector: 'app-list-runs',
-//   templateUrl: './list-runs.component.html',
-//   styleUrls: ['./list-runs.component.css']
-// })
-// export class ListRunsComponent implements OnInit {
-//
-//   constructor(private api: ApiService) { }
-//
-//   ngOnInit() {
-//
-//   }
-// }
-
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Component({
-    selector: 'embedded-sortable',
-    template: `
-        <h4>Move items between multi list sortable containers</h4>
-        <div class="row">
-            <div class="col-sm-3">
-                Drag Containers <input type="checkbox" [(ngModel)]="dragOperation"/>
-                <div >
-                    <div class="col-sm3"
-                         *ngFor="let container of containers; let i = index">
-                        <div class="panel panel-warning"
-                             dnd-sortable-container [sortableData]="container.widgets" [dropZones]="['widget-dropZone']">
-                            <div class="panel-heading">
-                                {{container.id}} - {{container.name}}
-                            </div>
-                            <div class="panel-body">
-                                <ul class="list-group">
-                                    <li *ngFor="let widget of container.widgets; let x = index" class="list-group-item"
-                                        dnd-sortable [sortableIndex]="x" [dragEnabled]="!dragOperation"
-                                        [dragData]="widget">{{widget.name}}</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`
+  selector: 'app-list-runs',
+  templateUrl: './list-runs.component.html',
+  styleUrls: ['./list-runs.component.css']
 })
-export class ListRunsComponent {
-    dragOperation: boolean = false;
+export class ListRunsComponent implements OnInit {
 
-    containers: Array<Container> = [
-        new Container(1, 'Container 1', [new Widget('1'), new Widget('2')]),
-        new Container(2, 'Container 2', [new Widget('3'), new Widget('4')]),
-        new Container(3, 'Container 3', [new Widget('5'), new Widget('6')])
-    ];
+  constructor(private api: ApiService) { }
 
-    widgets: Array<Widget> = [];
-    addTo($event: any) {
-        if ($event) {
-            this.widgets.push($event.dragData);
-        }
-    }
-}
+  ngOnInit() {
 
-class Container {
-    constructor(public id: number, public name: string, public widgets: Array<Widget>) {}
-}
-
-class Widget {
-    constructor(public name: string) {}
+  }
 }
--- core/src/main/resources/ui/src/app/components/steps/edit/parent/parent.component.ts
+++ core/src/main/resources/ui/src/app/components/steps/edit/parent/parent.component.ts
@@ -1,39 +1,52 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService, Step, StepType} from "../../../../services/api.service";
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-parent',
-  templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+    selector: 'app-parent',
+    templateUrl: './parent.component.html',
+    styleUrls: ['./parent.component.css']
 })
 export class EditStepParentComponent implements OnInit {
 
-  constructor(private api: ApiService, private route: ActivatedRoute, private router: Router) { }
+    constructor(private api: ApiService, private route: ActivatedRoute, private router: Router) {
+    }
 
-  step: Step;
+    step: Step;
 
-  StepType = StepType;
+    StepType = StepType;
 
-  allSteps: Step[];
+    allSteps: Step[];
 
-  ngOnInit() {
-      this.route.paramMap.subscribe(x => {
-          let stepId = x.get('id');
+    ngOnInit() {
+        this.route.paramMap.subscribe(x => {
+            let stepId = x.get('id');
 
-          this.api.getStep(stepId).subscribe(s => {
-              this.step = s;
+            this.api.getStep(stepId).subscribe(s => {
+                this.step = s;
 
-              this.api.listSteps().subscribe(st => {
-                  this.allSteps = st;
-              })
-          })
-      })
-  }
+                this.api.listSteps().subscribe(st => {
+                    this.allSteps = st;
 
-  save() {
-      this.api.updateStep(this.step).subscribe(x => {
-          this.router.navigateByUrl("/steps/details/" + this.step.id)
-      })
-  }
+                    this.sortSteps()
+                })
+            })
+        })
+    }
+
+    private sortSteps() {
+        this.allSteps.sort((a, b) => a.name.localeCompare(b.name))
+    }
+
+    reAddToAll(event: any) {
+        this.allSteps.push(event);
+
+        this.sortSteps()
+    }
+
+    save() {
+        this.api.updateStep(this.step).subscribe(x => {
+            this.router.navigateByUrl("/steps/details/" + this.step.id)
+        })
+    }
 }
